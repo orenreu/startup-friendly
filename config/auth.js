2b59e36eef23877188feddc17abcf0833b59feb7
Oren Reuveni <orenreu@gmail.com>
2016-11-10 18:08:53 +0200

    change facebook auth path

--- config/auth.js
+++ config/auth.js
@@ -10,24 +10,51 @@
 // config/auth.js
 
 // expose our config directly to our application using module.exports
-module.exports = {
+if (process.env.NODE_ENV == 'production') {
 
-    'facebookAuth' : {
-        'clientID'      : '1100217973333725', // your App ID
-        'clientSecret'  : '12b3922d9ad1473373fc13ee982fe7c1', // your App Secret
-        'callbackURL'   : 'http://startup-friendly.com/auth/facebook/callback'
-    },
+    module.exports = {
 
-    'twitterAuth' : {
-        'consumerKey'       : 'your-consumer-key-here',
-        'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
-    },
+        'facebookAuth': {
+            'clientID': '1100217973333725', // your App ID
+            'clientSecret': '12b3922d9ad1473373fc13ee982fe7c1', // your App Secret
+            'callbackURL': 'http://startup-friendly.com/auth/facebook/callback'
+        },
 
-    'googleAuth' : {
-        'clientID'      : 'your-secret-clientID-here',
-        'clientSecret'  : 'your-client-secret-here',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'twitterAuth': {
+            'consumerKey': 'your-consumer-key-here',
+            'consumerSecret': 'your-client-secret-here',
+            'callbackURL': 'http://localhost:3000/auth/twitter/callback'
+        },
+
+        'googleAuth': {
+            'clientID': 'your-secret-clientID-here',
+            'clientSecret': 'your-client-secret-here',
+            'callbackURL': 'http://localhost:3000/auth/google/callback'
+        }
+
     }
 
-};
+} else {
+
+    module.exports = {
+
+        'facebookAuth': {
+            'clientID': '1100217973333725', // your App ID
+            'clientSecret': '12b3922d9ad1473373fc13ee982fe7c1', // your App Secret
+            'callbackURL': 'http://localhost:8100/auth/facebook/callback'
+        },
+
+        'twitterAuth': {
+            'consumerKey': 'your-consumer-key-here',
+            'consumerSecret': 'your-client-secret-here',
+            'callbackURL': 'http://localhost:3000/auth/twitter/callback'
+        },
+
+        'googleAuth': {
+            'clientID': 'your-secret-clientID-here',
+            'clientSecret': 'your-client-secret-here',
+            'callbackURL': 'http://localhost:3000/auth/google/callback'
+        }
+
+    }
+}
